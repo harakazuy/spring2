f848d4e0e710a6de8fda8afae1f854d0f50a2a9f
harakazuy <gug919@yahoo.co.jp>
2016-02-09 22:44:37 +0900

    spring web sample

---
+++ MySampleWebApp1/src/main/java/jp/hara/spring/websample1/MySampleServlet.java
@@ -0,0 +1,49 @@
+package jp.hara.spring.websample1;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+/**
+ * Servlet implementation class MySampleServlet
+ */
+public class MySampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private MyBean mybean1;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init();
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("mybean", mybean1);
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String message = request.getParameter("message");
+		mybean1.addMessage(message);
+		response.sendRedirect("sample");
+	}
+
+}
